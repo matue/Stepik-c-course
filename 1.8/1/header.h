2f6bc48c64a6e8c0f7bd48af71d8572207c86dde
Nikita <nik.matue@gmail.com>
2018-10-21 15:47:52 +0500

    Initial commit

---
+++ 1.8/1/header.h
@@ -0,0 +1,2 @@
+int power(int x, unsigned p); //объявление (definition) функции power
+
---
+++ 1.8/1/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "header.h"
+
+int main()
+{
+    int a = power(5,4);
+    std::cout << a;
+}
+
