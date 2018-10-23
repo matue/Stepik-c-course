1bcb83fb0fa92f236a710a37664184bf1f28f0ec
Nikita <nik.matue@gmail.com>
2018-10-23 22:34:23 +0500

    Update 2.2/1/main.cpp

--- 2.2/1/main.cpp
+++ 2.2/1/main.cpp
@@ -4,21 +4,13 @@ using namespace std;
 
 unsigned gcd(unsigned a, unsigned b)
 {
-    if (a==0) {
-        return b;
-    }
-    else if (b==0) {
-        return a;
-    }
+    if (a==0) return b;
+    else if (b==0) return a;
     unsigned mx = (a>b)?a:b;
     unsigned mn = (a<b)?a:b;
     unsigned x = (mx % mn);
-    if (x==0) {
-        return mn;
-    }
-    else {
-        gcd(x, mn);
-    }
+    if (x==0) return mn;
+    else gcd(x, mn);
 }
 
 int main()
